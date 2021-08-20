55bd8e2b423461831fbe94ead2803160ca67d71a
Pat Laster <palaster307@gmail.com>
2021-08-20 18:15:05 -0500

    Fixed recipes and server crash

--- src/main/java/palaster/gj/items/HandItem.java
+++ src/main/java/palaster/gj/items/HandItem.java
@@ -1,13 +1,14 @@
 package palaster.gj.items;
 
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickItem;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import palaster.gj.api.capabilities.rpg.IRPG;
 import palaster.gj.api.capabilities.rpg.RPGCapability.RPGProvider;
 import palaster.gj.jobs.JobBloodSorcerer;
@@ -15,23 +16,23 @@ import palaster.libpal.items.SpecialModItem;
 
 public class HandItem extends SpecialModItem {
 
-	public HandItem(Properties properties, ResourceLocation resourceLocation) {
-		super(properties, resourceLocation, 0);
-		MinecraftForge.EVENT_BUS.register(this);
-	}	
-
-	@SubscribeEvent
-	public void onPlayerRightClickItem(RightClickItem e) {
-		if(!e.getPlayer().level.isClientSide)
-			if(e.getHand() == Hand.MAIN_HAND)
-				if(!e.getPlayer().getItemInHand(e.getHand()).isEmpty() && e.getPlayer().getItemInHand(e.getHand()).getItem() instanceof SwordItem)
-					if(!e.getPlayer().getItemInHand(Hand.OFF_HAND).isEmpty() && e.getPlayer().getItemInHand(Hand.OFF_HAND).getItem() == this) {
-						LazyOptional<IRPG> lazy_optional_rpg = e.getPlayer().getCapability(RPGProvider.RPG_CAPABILITY, null);
-						final IRPG rpg = lazy_optional_rpg.orElse(null);
-						if(rpg != null && rpg.getJob() != null && rpg.getJob() instanceof JobBloodSorcerer) {
-							e.getPlayer().hurt(DamageSource.MAGIC, 1F);
-							((JobBloodSorcerer) rpg.getJob()).addBlood(e.getPlayer(), 10);
-						}
+	public HandItem(Properties properties, ResourceLocation resourceLocation) { super(properties, resourceLocation, 0); }	
+	
+	@Override
+	public ActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+		if(!world.isClientSide)
+			if(hand == Hand.OFF_HAND) {
+				ItemStack mainHandItemStack = playerEntity.getItemInHand(Hand.MAIN_HAND);
+				if(!mainHandItemStack.isEmpty() && mainHandItemStack.getItem() instanceof SwordItem) {
+					LazyOptional<IRPG> lazy_optional_rpg = playerEntity.getCapability(RPGProvider.RPG_CAPABILITY, null);
+					final IRPG rpg = lazy_optional_rpg.orElse(null);
+					if(rpg != null && rpg.getJob() != null && rpg.getJob() instanceof JobBloodSorcerer) {
+						playerEntity.hurt(DamageSource.MAGIC, 1F);
+						((JobBloodSorcerer) rpg.getJob()).addBlood(playerEntity, 10);
+						return ActionResult.success(playerEntity.getItemInHand(hand));
 					}
+				}
+			}
+		return super.use(world, playerEntity, hand);
 	}
 }
